6c7e62837360d0213926bb53faef3673ac181b27
Ritik1325 <ritikrajput2550@gmail.com>
2025-08-16 14:29:26 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://job-portal-backend-2ula.onrender.com", 
+  baseURL:import.meta.env.VITE_API_URL, 
   withCredentials:true,
 });
 
