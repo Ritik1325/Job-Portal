8110295f16a339abfe4b7e72998df87cd5621b4a
Ritik1325 <ritikrajput2550@gmail.com>
2025-08-16 13:53:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -46,21 +46,6 @@ app.get('/',(req,res)=>{
 
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-  });
-}
-
-
-
-
-
-
 
 const server=async ()=>{
     try {
