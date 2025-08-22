d9c0f3a53dda96557ed78ccdaa831f4346fd6473
Ritik Kumar Singh <ritikrajput2550@gmail.com>
2025-08-22 17:37:07 +0530

    chnage

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,13 @@
 import express from 'express';
 import protect from '../middleware/protect.js';
 import checkRole from '../middleware/checkRole.js';
-import userAsRole, { getUser } from '../controllers/userController.js';
+import  { UserAsRole } from '../controllers/userController.js';
 
 
 const router=express.Router();
 
 
-router.get('/userole',protect,checkRole('employer','jobseeker'),userAsRole);
-router.get('/user',getUser);
-
+router.get('/user',protect,checkRole('employer','jobseeker'),UserAsRole);
 
 
 
