b030c86598b1c397da691ef1a3500e6ce9b1f5dd
Ritik Kumar Singh <ritikrajput2550@gmail.com>
2025-08-22 15:47:44 +0530

    change

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ import connect from './config/connect.js';
 import dotenv from 'dotenv'
 import fs from 'fs';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 
 
@@ -19,26 +22,39 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 app.use(cookieParser())
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const routesPath = path.join(__dirname, "routes");
+const routefiles = fs.readdirSync(routesPath);
 
 
 
+// routefiles.forEach(file=>{
 
-const routefiles=fs.readdirSync('./routes');
+//     import(`./routes/${file}`).then((route)=>{
+//         console.log("Loaded route file:", file);
+//         app.use(`/job-portal/`,route.default)
+//     }).catch(err=>{
+//         console.log(`error importing ${file}`,err.message);
+//     })
+// })
 
-routefiles.forEach(file=>{
-
-    import(`./routes/${file}`).then((route)=>{
-        console.log("Loaded route file:", file);
-        app.use(`/job-portal/`,route.default)
-    }).catch(err=>{
-        console.log(`error importing ${file}`,err.message);
-    })
-})
+routefiles.forEach((file) => {
+  import(path.join(routesPath, file)).then((route) => {
+    console.log("✅ Loaded route file:", file);
+    app.use("/job-portal", route.default);
+  }).catch(err => {
+    console.log(`❌ Error importing ${file}:`, err.message);
+  });
+});
 
 
 app.get('/',(req,res)=>{
